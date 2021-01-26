932fb4e8646c8d1b158955975b853edfcb4e5edd
RaulTejero <raultejeromartos@gmail.com>
2021-01-26 11:55:27 +0100

    footer

--- webCursos/src/app/app-routing.module.ts
+++ webCursos/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CursoJSComponent } from './components/curso-js/curso-js.component';
-import { HomeComponent } from './components/home/home.component';
+import { CursosComponent } from './components/cursos/cursos.component';
 import { MyPersonComponent } from './components/my-person/my-person.component';
+import { PorfolioComponent } from './components/porfolio/porfolio.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "home", component: HomeComponent},
+  {path: "", component: PorfolioComponent},
+  {path: "porfolio", component: PorfolioComponent},
   {path: 'myPerson', component: MyPersonComponent},
-  {path: "home/cursoJS", component: CursoJSComponent},
+  {path: "cursos", component: CursosComponent},
 ];
 
 @NgModule({
--- webCursos/src/app/app.module.ts
+++ webCursos/src/app/app.module.ts
@@ -3,23 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+
 import { MyPersonComponent } from './components/my-person/my-person.component';
 import { NavComponent } from './components/nav/nav.component';
 import { MaxLenghtPipe } from './pipes/max-lenght.pipe';
 import { CursoJSComponent } from './components/curso-js/curso-js.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { PorfolioComponent } from './components/porfolio/porfolio.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     MyPersonComponent,
     NavComponent,
     MaxLenghtPipe,
     CursoJSComponent,
     FooterComponent,
+    CursosComponent,
+    PorfolioComponent,
   ],
   imports: [
     BrowserModule,
